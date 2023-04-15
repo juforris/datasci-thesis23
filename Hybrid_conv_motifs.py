5e617ca8f462e2a200bd22e246225b67a73c02ca
Julie Wollebæk Førrisdal <juforris@nmbu.no>
2023-04-15 19:42:58 +0200

    Convolution motifs

---
+++ Hybrid_conv_motifs.py
@@ -0,0 +1,62 @@
+# Import modules
+import pickle
+import pandas as pd
+import numpy as np
+
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import tensorflow as tf
+from tensorflow.keras import backend as K
+tf.random.set_seed(1337)  # sets seeds for base-python, numpy and tf
+
+# Validation set
+maxlen = 200
+X_valid10bm = pd.read_pickle("data10bm/X_valid.pkl")
+X_valid = pad_sequences(X_valid10bm, padding="post", maxlen=maxlen)
+
+# Hybrid model trained on four data subsets
+models = []
+models.append(load_model("Hybrid10bm/Hybrid.h5"))
+models.append(load_model("Hybrid25bm/Hybrid.h5"))
+models.append(load_model("Hybrid50bm/Hybrid.h5"))
+models.append(load_model("Hybrid100bm/Hybrid.h5"))
+
+# First conv layer specs
+nfilters = 224
+kernel = 9
+dims = 4 
+
+# Extract motifs from first convolution layer of each model
+for model, name in zip(models, ["10bm", "25bm", "50bm", "100bm"]):
+    # First convolution layer
+    conv_layer = model.layers[0]
+    conv_input = conv_layer.input
+    conv_output = conv_layer.get_output_at(0)
+    # Function to find max activations and corresponding position index
+    f = K.function([conv_input], [K.argmax(conv_output, axis=1), K.max(conv_output, axis=1)])
+    # Create motifs
+    motifs = np.zeros((nfilters, kernel, dims))
+    nsites = np.zeros(nfilters)
+    # Batches of 100 from X_valid
+    for i in range(0, len(X_valid), 100):
+        x = X_valid[i:i+100]
+        # Pass batch through conv layer
+        max_inds, max_acts = f([x])
+        for m in range(nfilters):
+            for n in range(len(x)):
+                if max_acts[n, m] > 0:
+                    nsites[m] += 1
+                    motif = x[n, max_inds[n, m]:max_inds[n, m] + kernel, :]
+                    motifs[m] += motif
+                    
+    motifs_file = open("HybridConvMotifs/motifs"+name+".txt", "w")
+    motifs_file.write("MEME version 5.5.1\n\nALPHABET= ACGT\n\nstrands: + -\n\nBackground letter frequencies (from uniform background):\nA 0.25000 C 0.25000 G 0.25000 T 0.25000\n\n")
+    for m in range(nfilters):
+        if nsites[m] == 0:
+            continue
+        motifs_file.write("MOTIF M%i O%i\n" % (m, m))
+        motifs_file.write("letter-probability matrix: alength= 4 w= %i nsites= %i E= 1337.0e-6\n" % (kernel, nsites[m]))
+        for j in range(kernel):
+            motifs_file.write("%f %f %f %f\n" % tuple(1.0 * motifs[m, j, 0:dims] / np.sum(motifs[m, j, 0:dims])))
+        motifs_file.write("\n")
+    motifs_file.close()
---
+++ Hybrid_deepExplainer.py
@@ -0,0 +1,50 @@
+# Import modules
+import pandas as pd
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import shap
+from deeplift.dinuc_shuffle import dinuc_shuffle
+
+# Load validation sequences
+X_valid = pd.read_pickle("data/X_valid.pkl")
+
+# Input length
+maxlen = 200
+
+# Load model
+model = load_model("Hybrid.h5")
+
+# Preprocessing
+data = X_valid
+one_hot = pad_sequences(data, padding="post", maxlen=maxlen)
+
+
+### DeepExplainer functions
+# Source: https://github.com/bernardo-de-almeida/DeepSTARR/blob/main/DeepSTARR/DeepSTARR_nucl_contr_scores.py
+def dinuc_shuffle_several_times(list_containing_input_modes_for_an_example, seed=1234):
+    dinuc_shuffle_n=100
+    onehot_seq = list_containing_input_modes_for_an_example[0]
+    rng = np.random.RandomState(seed)
+    to_return = np.array([dinuc_shuffle(onehot_seq, rng=rng) for i in range(dinuc_shuffle_n)])
+    return [to_return] #wrap in list for compatibility with multiple modes
+
+def my_deepExplainer(model, one_hot):
+    explainer = shap.DeepExplainer((model.layers[0].input, model.layers[-1].output), data=dinuc_shuffle_several_times)
+    shap.explainers._deep.deep_tf.op_handlers["AddV2"] = shap.explainers._deep.deep_tf.passthrough
+    # running on all sequences
+    shap_values_hypothetical = explainer.shap_values(one_hot, check_additivity=False)
+    # normalising contribution scores
+    # sum the deeplift importance scores across the ACGT axis (different nucleotides at the same position)
+    # and “project” that summed importance onto whichever base is actually present at that position
+    shap_values_contribution = shap_values_hypothetical[0]*one_hot
+    return shap_values_hypothetical[0], shap_values_contribution
+
+# Run DeepExplainer
+scores = my_deepExplainer(model, one_hot)
+shap_values_hypothetical, shap_values_contribution = scores
+
+# Save scores
+np.save("modisco/contribution_scores.npy", shap_values_contribution)
+np.save("modisco/hypothetical_scores.npy", shap_values_hypothetical)
+np.save("modisco/10bm_valid_one_hot.npy", one_hot)
